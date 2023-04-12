1e0ad7850ddc9b3170d97c851044e53ce9d83767
Billy Mohajeri <billy.mohajeri@gmail.com>
2023-04-12 15:54:22 +0300

    Install CanCanCan and config ability file

---
+++ app/models/ability.rb
@@ -0,0 +1,16 @@
+# frozen_string_literal: true
+
+class Ability
+  include CanCan::Ability
+
+  def initialize(user)
+    user ||= User.new
+    if user.role == 'admin'
+      can :manage, :all
+    else
+      can :manage, Post, author_id: user.id
+      can :manage, Comment, author_id: user.id
+      can :read, :all
+    end
+  end
+end
